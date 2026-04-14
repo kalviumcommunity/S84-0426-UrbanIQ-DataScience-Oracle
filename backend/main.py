cfc827cb153b76184888f7d6be1825c9780d4a32
unknown <aryamanworks20@gmail.com>
2026-04-14 16:27:14 +0530

    Updated Main.py file

---
+++ backend/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from database.connection import connect_to_mongo, close_mongo_connection
+from config.settings import settings
+
+# Define the lifespan of the FastAPI app to manage startup and shutdown events
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # What happens on startup
+    await connect_to_mongo()
+    yield
+    # What happens on shutdown
+    await close_mongo_connection()
+
+# Initialize the FastAPI App
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description="Backend for Argus: Municipal Grievance Analytics System",
+    version="1.0.0",
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://s84-oracle.netlify.app"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Global Exception Handler
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    return JSONResponse(
+        status_code=500,
+        content={"status": "error", "message": "An unexpected error occurred.", "details": str(exc)},
+    )
+
+# Import and include routers
+from routes.insights import router as insights_router
+app.include_router(insights_router, prefix="/api/v1")
+
+# Test route to verify server is running
+@app.get("/")
+async def root():
+    return {
+        "status": "success",
+        "message": f"Welcome to the {settings.PROJECT_NAME}",
+        "database": settings.DATABASE_NAME
+    }
+
